26bbbe633b83b219f065e79f353f904b368d92b5
Prince Boye <boyeabbey@gmail.com>
2023-03-21 17:51:46 -0700

    putchar

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+ * times_table - prints the 9 times table starting with 0
+ */
+void times_table(void)
+{
+	int i, j, result;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		
