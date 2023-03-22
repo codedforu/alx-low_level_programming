d67a1d9bcc061b3db0ec94986d66d6b34cde3f97
Prince Boye <boyeabbey@gmail.com>
2023-03-21 19:27:53 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,16 +2,16 @@
 
 /**
  * print_alphabet_x10 - Prints the lowercase alphabet 10 times, followed by a new line each time
- *
- * Return: void
  */
 void print_alphabet_x10(void)
 {
-	int i = 0;
-
-	while (i < 10)
+	int i, j;
+	for (i = 0; i < 10; i++)
 	{
-		print_alphabet();
-		i++;
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
 	}
 }
