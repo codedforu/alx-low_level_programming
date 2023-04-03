117fb9ae84c23139c154dacfff747fe1de7862be
Prince Boye <boyeabbey@gmail.com>
2023-04-03 09:19:39 -0700

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,10 +2,10 @@
 /**
  *_memcpy - a function that copies memory area
  *@dest: memory where is stored
- *@src: memory where is copied
- *@n: number of bytes
+ *@src: copies where memory is
+ *@n: number of bytes from memory
  *
- *Return: copied memory with n bytes changed
+ *Return: copied memory with n bytes
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
